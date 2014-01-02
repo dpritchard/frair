f5079226442b584177e7a7783fb3892d13143e27
Daniel Pritchard <daniel@pritchard.co>
2014-01-03 08:48:02 +1000

    Split frair_methods

---
+++ R/frair_fit_methods.R
@@ -0,0 +1,25 @@
+## frair methods
+
+# Print for objects of class frfit
+print.frfit <- function(x, ...){
+    cat('FUNCTIONAL RESPONSE FIT\n')
+    cat(paste0('\nResponse:            ', x$response))
+    cat(paste0('\nDescription:         ', as.character(frair_responses(show=FALSE)[[x$response]][2])))
+    cat(paste0('\nOptimised variables: ', paste0(x$optimvars, collapse=', ')))
+    cat(paste0('\nFixed variables:     ', ifelse(test=!is.null(x$fixedvars), yes=paste(x$fixedvars, collapse=', '), no='NA')))
+    cat('\n')
+    cat('\nCoefficients:\n')
+    print(round(x$coefficients, 3))
+    cat('\nNOTE: It is recomended you inspect the raw fit too (see: ?frair_fit)')
+}
+
+plot.frfit <- function(x, xlab=x$xvar, ylab=x$yvar, ...){
+    plot(x$x, x$y, xlab=xlab, ylab=ylab, ...)
+}
+
+lines.frfit <- function(x, ...){
+    newx <- seq(from=0, to=max(x$x), by=1)
+    fitfun <- get(x$response)
+    newy <- fitfun(newx, as.list(x$coefficients))
+    lines(newx, newy, ...)
+}
