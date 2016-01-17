1412266d53449dbf7e40a9da392490e895274f9e
Daniel Pritchard <daniel@pritchard.co>
2016-01-17 19:17:44 +1300

    Minor updates to flexpnr testing

--- tests/testthat/test_flexpnr.R
+++ tests/testthat/test_flexpnr.R
@@ -14,15 +14,15 @@ test_that("friar_fit understands a flexpnr response", {
 })
 
 test_that("flexpnr collapses to rogerII when q = 0", {
-    expofit <- frair_fit(eaten~density, data = pulex, 
-                         response = 'flexpnr', start = list(b = 1.2, h = 0.015), 
-                         fixed = list(T = 1, q = 0))
+    expfit <- frair_fit(eaten~density, data = pulex, 
+                        response = 'flexpnr', start = list(b = 1.2, h = 0.015), 
+                        fixed = list(T = 1, q = 0))
     rogfit <- frair_fit(eaten~density, data=pulex, 
                         response = 'rogersII', start = list(a = 1.2, h = 0.015), 
                         fixed=list(T = 1))
     # NB: Use expect_equivalent for check.attributes = FALSE
-    expect_equivalent(coef(expofit)['b'], coef(rogfit)['a']) 
-    expect_equal(coef(expofit)['h'], coef(rogfit)['h'])
+    expect_equivalent(coef(expfit)['b'], coef(rogfit)['a']) 
+    expect_equal(coef(expfit)['h'], coef(rogfit)['h'])
 })
 
 
